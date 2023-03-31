2e75f352043ba308e4e797d2ed08db46df62780c
yamadamasashi <shisama94@gmail.com>
2023-03-31 09:25:03 +0900

    そう復習

--- sample-app/features/components/Form.tsx
+++ sample-app/features/components/Form.tsx
@@ -1,13 +1,10 @@
-import { FC, useState, Dispatch, SetStateAction } from 'react'
+import { FC } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
+import type { Inputs } from '../types'
 
 // type inputNames = {
 //   [key: string]: Dispatch<SetStateAction<string | undefined>>
 // }
-
-type Inputs = {
-  [key: string]: string // これにしないとerrorが出る
-}
 
 const Form: FC = () => {
   // const [mail, setMail] = useState<string | undefined>()
@@ -24,7 +21,6 @@ const Form: FC = () => {
   // const handleSubmit = () => {
   //   console.log(mail, pass)
   // }
-
   const {
     register,
     handleSubmit,
@@ -32,17 +28,29 @@ const Form: FC = () => {
     formState: { errors }
   } = useForm()
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data, errors)
 
   console.log(watch('mail'))
   console.log(watch('pass'))
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input defaultValue='test' {...register('mail')} />
-      <input defaultValue='test' {...register('pass', { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.pass && <span>This field is required</span>}
+      <label htmlFor=''>
+        <input defaultValue='' {...register('mail', { required: true })} />
+      </label>
+      {errors.mail && <span>This field is mail</span>}
+      <label htmlFor=''>
+        <input defaultValue='' {...register('pass', { required: true })} />
+      </label>
+      {errors.pass && <span>This field is pass</span>}
+      <label htmlFor=''>
+        <select {...(register('age'), { required: true })}>
+          <option value='10~20代'>10~20代</option>
+          <option value='30~40代'>30~40代</option>
+          <option value='50~'>50~</option>
+        </select>
+      </label>
+      {errors.age && <span>This field is required</span>}
 
       <input type='submit' />
     </form>
