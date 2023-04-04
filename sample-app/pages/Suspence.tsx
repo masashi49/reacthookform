8dd38a0f5a6d00966bf5eb0710024cfd7f04b2cd
yamadamasashi <shisama94@gmail.com>
2023-04-05 01:00:54 +0900

    suspendを試す

--- sample-app/pages/Suspence.tsx
+++ sample-app/pages/Suspence.tsx
@@ -5,10 +5,24 @@ import axios from 'axios'
 import { flushSync } from 'react-dom'
 import { resolve } from 'path'
 
+type Props = {
+  [key: string]: string
+}
+
 const sleep = () => {
   return new Promise((resolve) => {
     setTimeout(resolve, 1000)
   })
+}
+
+export const RenderingNotifier = ({ name }: Props) => {
+  console.log(`${name} rendered`)
+  return null
+}
+
+const fetchData1 = async () => {
+  await sleep()
+  return `Hello, ${(Math.random() * 1000).toFixed(0)}`
 }
 
 export const Suspence = () => {
@@ -16,14 +30,6 @@ export const Suspence = () => {
     throw sleep()
   }
   return <p>Hello, world!</p>
-}
-
-type Props = {
-  [key: string]: string
-}
-export const RenderingNotifier = ({ name }: Props) => {
-  console.log(`${name} rendered`)
-  return null
 }
 
 const SuspenceDemo = () => {
@@ -43,4 +49,31 @@ const SuspenceDemo = () => {
   )
 }
 
-export default SuspenceDemo
+const Loader = () => {
+  const [loading, setloading] = useState(false)
+  const [data, setData] = useState<string | null>(null)
+
+  if (data === null) {
+    throw fetchData1().then(setData)
+  }
+  return (
+    <div>
+      <div>
+        <button onClick={() => setloading(true)}>load</button>
+      </div>
+    </div>
+  )
+}
+const SuspenceDemo2 = () => {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      Suspenseを試す
+      <Suspense fallback={<p>Loading...</p>}>
+        <Loader />
+      </Suspense>
+    </div>
+  )
+}
+
+export default SuspenceDemo2
